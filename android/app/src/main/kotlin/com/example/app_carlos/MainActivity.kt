4b068d60d304003a10b5edbdac5cd29fd479b858
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2020-09-07 16:54:00 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/com/example/app_carlos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_carlos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuTeste());
+}
+
+class MeuTeste extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(
+        title: Text("App do Carlos")
+      ),
+      body: Text("Hello World!!!")
+      )
+    );
+    
+  }
+}
